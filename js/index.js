03be1538b54e4241183dfe109164d34b00c01656
kanngji <50470748+kanngji@users.noreply.github.com>
2022-08-23 21:15:48 +0900

    팝업창 , index.html 섹션 하나 추가

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ $(function () {
     infinite: true, //무한 반복 옵션
     speed: 100, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간
     arrows: true, // 옆으로 이동하는 화살표 표시 여부
-    dots: true, //스크롤바 아래 점으로 페이지네이션 여부
+    // dots: true, //스크롤바 아래 점으로 페이지네이션 여부
     autoplay: true, //자동 스크롤 사용 여부
     autoplaySpeed: 10000, //자동 스크롤시 다음으로 넘어가는데 걸리는 시간
     prevArrow: "<button type='button' class='slick-prev'>Previous</button>", // 이전 화살표 모양 설정
@@ -12,4 +12,7 @@ $(function () {
   });
 });
 
-console.log("얍");
+//팝업창 세팅
+window.onload = function () {
+  window.open("popup.html", "popupNo1", "width=300,height=360");
+};
