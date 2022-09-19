0a2672f1fd40fce67ef1f6599d5b0d09501f6b45
Victory-soo <victory-soo@naver.com>
2022-09-19 21:29:03 +0900

    Soo

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: ["airbnb-base"],
+    rules: {
+        "linebreak-style": 0,
+        "no-console": "off",
+        "max-classes-per-file": "off",
+        "prefer-destructuring": "off",
+    },
+    //   lintOnSave: false
+    //   eslint 미적용
+};
